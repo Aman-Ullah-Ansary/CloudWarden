e044973e64efb8422231c0e30092318058c5a280
Aman Ullah Ansary <write2amanansary@gmail.com>
2026-08-02 17:42:39 +0530

    Add AI Incident

---
+++ agent/incident_ai.py
@@ -0,0 +1,36 @@
+import os
+from groq import Groq
+
+class IncidentAI:
+
+    def __init__(self):
+        self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+    def analyze(self, metrics):
+
+        prompt = f"""
+You are a Senior Kubernetes Site Reliability Engineer.
+
+Analyze these metrics:
+
+{metrics}
+
+Provide:
+
+1. Root Cause
+2. Impact
+3. Recommendation
+4. Severity
+"""
+
+        response = self.client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        )
+
+        return response.choices[0].message.content
